29073c494df761d744632a2c0e94576953569a0c
Ulisesgtz33 <51324868+Ulisesgtz33@users.noreply.github.com>
2021-03-19 00:33:24 -0600

    Código con acceso a la base de datos y diferenciación en Latam

---
+++ Vacunas.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+Vacunastotales <- read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
+
+latam <- c("Mexico", "Argentina", 
+           "Colombia", "Chile", 
+           "Brazil", "Bolivia", 
+           "Costa Rica", "Ecuador",
+           "Guatemala", "Panama", 
+           "Paraguay", "Peru", 
+           "Puerto Rico", "Dominican Republic")
+
+Vacunas_latam <- Vacunastotales %>%
+  select( date, location, total_vaccinations, 
+          total_vaccinations_per_hundred, 
+          daily_vaccinations_per_million) %>%
+  filter(location %in% latam)
+
+p <- ggplot(data = Vacunas_latam) + 
+  geom_line(mapping = aes(x = date, y = total_vaccinations_per_hundred, color = location))
+
+plotly::ggplotly(p)
