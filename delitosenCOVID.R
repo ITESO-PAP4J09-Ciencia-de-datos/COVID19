937ed6dc0a8129d891d3513b6677636b3d7bf29d
Pablo <pbenavides@iteso.mx>
2021-03-23 17:13:08 -0600

    Optimización de código

--- delitosenCOVID.R
+++ delitosenCOVID.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 library(lubridate)
-library(tibbletime)
+# library(tibbletime)
 library(reactable)
 library(htmltools)
+
+library(tsibble)
+library(feasts)
+library(fable)
 
 ####Importación de datos####
 delitos <- read_csv("delitos2015-2021.csv",
@@ -20,7 +24,7 @@ delitos_a_comparar <- c("Feminicidio", "Abuso sexual",
                         "Tráfico de menores", "Secuestro", "Violencia familiar")
 
 delitos_tidy <- delitos %>%
-  select( Ano, Entidad, Tipo_de_delito, Subtipo_de_delito, Enero:Diciembre ) %>%
+  # select( Ano, Entidad, Tipo_de_delito, Subtipo_de_delito, Enero:Diciembre ) %>%
   filter( Tipo_de_delito %in% delitos_a_comparar | 
           Subtipo_de_delito == "Homicidio doloso" |
           Subtipo_de_delito == "Lesiones dolosas" ) %>% 
@@ -28,205 +32,233 @@ delitos_tidy <- delitos %>%
   cols = Enero:Diciembre ,
   names_to = "Meses",
   values_to = "Cuenta"
-)
+) %>% 
+  group_by(Ano, Meses, Tipo_de_delito, Subtipo_de_delito) %>% 
+  summarise(Cuenta = sum(Cuenta), .groups = "drop")
 
 #Poner los valores de cero como NA para quitarlos
-delitos_tidy[delitos_tidy==0] <- NA
+# delitos_tidy[delitos_tidy==0] <- NA
+glimpse(delitos_tidy)
+
+
+
+delitos_tidy <- delitos_tidy %>% 
+  mutate(
+    Meses = str_trunc(Meses, width = 3, ellipsis = ""),
+    Meses = case_when(
+      Meses == "Ene" ~ "Jan",
+      Meses == "Abr" ~ "Apr",
+      Meses == "Ago" ~ "Aug",
+      Meses == "Dic" ~ "Dec",
+      TRUE           ~ Meses
+    )
+  ) %>% 
+  unite(col = "Fecha", c(Ano,Meses), sep = " ") %>% 
+  mutate(Fecha = yearmonth(Fecha))
+
+delitos_tidy_tsbl <- delitos_tidy %>% 
+  as_tsibble(
+    index = Fecha,
+    key   = c(Tipo_de_delito, Subtipo_de_delito)
+  )
+
+delitos_tidy_tsbl %>% 
+  autoplot()
 
 #Hacer una columna para los meses y años en serie de tiempo
 
-delitos_tidy$Meses <- gsub("Enero", 1, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Febrero", 2, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Marzo", 3, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Abril", 4, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Mayo", 5, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Junio", 6, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Julio", 7, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Agosto", 8, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Septiembre", 9, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Octubre", 10, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Noviembre", 11, delitos_tidy$Meses)
-delitos_tidy$Meses <- gsub("Diciembre", 12, delitos_tidy$Meses)
-
-delitos_tidy <- delitos_tidy %>%
-  mutate( Fecha = make_date(Ano, Meses)) %>%
-  na.omit() %>%
-  select( Fecha, Tipo_de_delito, Cuenta)
+# delitos_tidy$Meses <- gsub("Enero", 1, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Febrero", 2, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Marzo", 3, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Abril", 4, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Mayo", 5, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Junio", 6, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Julio", 7, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Agosto", 8, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Septiembre", 9, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Octubre", 10, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Noviembre", 11, delitos_tidy$Meses)
+# delitos_tidy$Meses <- gsub("Diciembre", 12, delitos_tidy$Meses)
+# 
+# delitos_tidy <- delitos_tidy %>%
+#   mutate( Fecha = make_date(Ano, Meses)) %>%
+#   na.omit() %>%
+#   select( Fecha, Tipo_de_delito, Cuenta)
   
 
 
 ####Acumulados por delito en la misma fecha####
 
 #Feminicidio
-Acum_feminicidio <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Feminicidio")
-
-numfem <- as_tibble(
-  aggregate(Acum_feminicidio$Cuenta, by=list(Acum_feminicidio$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Feminicidio")
-
-#Homicidio doloso
-
-Acum_homicidio <- delitos_tidy %>%
-  filter(Tipo_de_delito == "Homicidio")
-
-numhom <- as_tibble(
-  aggregate(Acum_homicidio$Cuenta, by=list(Acum_homicidio$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Homicidio doloso")
-
-#Lesiones dolosas
-
-Acum_lesiones <- delitos_tidy %>%
-  filter(Tipo_de_delito == "Lesiones")
-
-numles <- as_tibble(
-  aggregate(Acum_lesiones$Cuenta, by=list(Acum_lesiones$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Lesiones dolosas")
-
-#Abuso sexual
-Acum_abuso <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Abuso sexual")
-
-numabuso <- as_tibble(
-  aggregate(Acum_abuso$Cuenta, by=list(Acum_abuso$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Abuso sexual")
-
-#Acoso sexual
-Acum_acoso <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Acoso sexual")
-
-numacoso <- as_tibble(
-  aggregate(Acum_acoso$Cuenta, by=list(Acum_acoso$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Acoso sexual")
-  
-#Hostigamiento sexual
-Acum_hosti <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Hostigamiento sexual")
-
-numhosti <- as_tibble(
-  aggregate(Acum_hosti$Cuenta, by=list(Acum_hosti$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Hostigamiento sexual")
-
-#Incesto
-
-Acum_incesto <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Incesto")
-
-numincesto <- as_tibble(
-  aggregate(Acum_incesto$Cuenta, by=list(Acum_incesto$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Incesto")
-
-#Otros delitos que atentan contra la libertad y la seguridad sexual
-
-Acum_otros <- delitos_tidy %>%
-  filter( 
-    Tipo_de_delito == "Otros delitos que atentan contra la libertad y la seguridad sexual")
-
-numotros <- as_tibble(
-  aggregate(Acum_otros$Cuenta, by=list(Acum_otros$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Otros delitos que atentan contra la libertad y la seguridad sexual")
-
-#Violación simple
-Acum_vs <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Violación simple")
-
-numvs <- as_tibble(
-  aggregate(Acum_vs$Cuenta, by=list(Acum_vs$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Violación simple")
-
-#violación equiparada
-Acum_ve <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Violación equiparada")
-
-numve <- as_tibble(
-  aggregate(Acum_ve$Cuenta, by=list(Acum_ve$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Violación equiparada")
-
-#Trata de personas
-Acum_trata <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Trata de personas")
-
-numtrata <- as_tibble(
-  aggregate(Acum_trata$Cuenta, by=list(Acum_trata$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Trata de personas")
-
-#Tráfico de menores
-Acum_trafi <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Tráfico de menores")
-
-numtrafi <- as_tibble(
-  aggregate(Acum_trafi$Cuenta, by=list(Acum_trafi$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Tráfico de menores")
-
-#Secuestro
-Acum_secu <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Secuestro")
-
-numsecu <- as_tibble(
-  aggregate(Acum_secu$Cuenta, by=list(Acum_secu$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Secuestro")
-
-#Violencia familiar
-Acum_viofam <- delitos_tidy %>%
-  filter( Tipo_de_delito == "Violencia familiar")
-
-numviofam <- as_tibble(
-  aggregate(Acum_viofam$Cuenta, by=list(Acum_viofam$Fecha), sum)) %>%
-  transmute(Fecha = Group.1, Cuenta = x) %>%
-  mutate(Delito =  "Violencia familiar")
+# Acum_feminicidio <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Feminicidio")
+# 
+# numfem <- as_tibble(
+#   aggregate(Acum_feminicidio$Cuenta, by=list(Acum_feminicidio$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Feminicidio")
+# 
+# #Homicidio doloso
+# 
+# Acum_homicidio <- delitos_tidy %>%
+#   filter(Tipo_de_delito == "Homicidio")
+# 
+# numhom <- as_tibble(
+#   aggregate(Acum_homicidio$Cuenta, by=list(Acum_homicidio$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Homicidio doloso")
+# 
+# #Lesiones dolosas
+# 
+# Acum_lesiones <- delitos_tidy %>%
+#   filter(Tipo_de_delito == "Lesiones")
+# 
+# numles <- as_tibble(
+#   aggregate(Acum_lesiones$Cuenta, by=list(Acum_lesiones$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Lesiones dolosas")
+# 
+# #Abuso sexual
+# Acum_abuso <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Abuso sexual")
+# 
+# numabuso <- as_tibble(
+#   aggregate(Acum_abuso$Cuenta, by=list(Acum_abuso$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Abuso sexual")
+# 
+# #Acoso sexual
+# Acum_acoso <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Acoso sexual")
+# 
+# numacoso <- as_tibble(
+#   aggregate(Acum_acoso$Cuenta, by=list(Acum_acoso$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Acoso sexual")
+#   
+# #Hostigamiento sexual
+# Acum_hosti <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Hostigamiento sexual")
+# 
+# numhosti <- as_tibble(
+#   aggregate(Acum_hosti$Cuenta, by=list(Acum_hosti$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Hostigamiento sexual")
+# 
+# #Incesto
+# 
+# Acum_incesto <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Incesto")
+# 
+# numincesto <- as_tibble(
+#   aggregate(Acum_incesto$Cuenta, by=list(Acum_incesto$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Incesto")
+# 
+# #Otros delitos que atentan contra la libertad y la seguridad sexual
+# 
+# Acum_otros <- delitos_tidy %>%
+#   filter( 
+#     Tipo_de_delito == "Otros delitos que atentan contra la libertad y la seguridad sexual")
+# 
+# numotros <- as_tibble(
+#   aggregate(Acum_otros$Cuenta, by=list(Acum_otros$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Otros delitos que atentan contra la libertad y la seguridad sexual")
+# 
+# #Violación simple
+# Acum_vs <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Violación simple")
+# 
+# numvs <- as_tibble(
+#   aggregate(Acum_vs$Cuenta, by=list(Acum_vs$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Violación simple")
+# 
+# #violación equiparada
+# Acum_ve <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Violación equiparada")
+# 
+# numve <- as_tibble(
+#   aggregate(Acum_ve$Cuenta, by=list(Acum_ve$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Violación equiparada")
+# 
+# #Trata de personas
+# Acum_trata <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Trata de personas")
+# 
+# numtrata <- as_tibble(
+#   aggregate(Acum_trata$Cuenta, by=list(Acum_trata$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Trata de personas")
+# 
+# #Tráfico de menores
+# Acum_trafi <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Tráfico de menores")
+# 
+# numtrafi <- as_tibble(
+#   aggregate(Acum_trafi$Cuenta, by=list(Acum_trafi$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Tráfico de menores")
+# 
+# #Secuestro
+# Acum_secu <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Secuestro")
+# 
+# numsecu <- as_tibble(
+#   aggregate(Acum_secu$Cuenta, by=list(Acum_secu$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Secuestro")
+# 
+# #Violencia familiar
+# Acum_viofam <- delitos_tidy %>%
+#   filter( Tipo_de_delito == "Violencia familiar")
+# 
+# numviofam <- as_tibble(
+#   aggregate(Acum_viofam$Cuenta, by=list(Acum_viofam$Fecha), sum)) %>%
+#   transmute(Fecha = Group.1, Cuenta = x) %>%
+#   mutate(Delito =  "Violencia familiar")
 
 #####Todos los delitos acumulados en un df####
 #Violencia familiar y lesiones dolosas tienen un rango mayor y mueven todo
-delitos_acumulados <- rbind(
-  numabuso,
-  numacoso,
-  numfem,
-  numhosti,
-  numincesto,
-  numotros,
-  numsecu,
-  numtrafi,
-  numtrata,
-  numve,
-  numvs,
-  numhom,
-  numles,
-  numviofam
-)
-
-delitos_sexuales_y_de_genero <- rbind(
-  numabuso,
-  numacoso,
-  numhosti,
-  numotros,
-  numvs,
-  numve,
-  numfem
-)
-
-delitos_libertad <- rbind (
-  numtrata,
-  numtrafi,
-  numsecu
-)
-
-delitos_dolosos <- rbind (
-  numhom,
-  numles
-)
+# delitos_acumulados <- rbind(
+#   numabuso,
+#   numacoso,
+#   numfem,
+#   numhosti,
+#   numincesto,
+#   numotros,
+#   numsecu,
+#   numtrafi,
+#   numtrata,
+#   numve,
+#   numvs,
+#   numhom,
+#   numles,
+#   numviofam
+# )
+# 
+# delitos_sexuales_y_de_genero <- rbind(
+#   numabuso,
+#   numacoso,
+#   numhosti,
+#   numotros,
+#   numvs,
+#   numve,
+#   numfem
+# )
+# 
+# delitos_libertad <- rbind (
+#   numtrata,
+#   numtrafi,
+#   numsecu
+# )
+# 
+# delitos_dolosos <- rbind (
+#   numhom,
+#   numles
+# )
 
 #####Graficas#### 
 
