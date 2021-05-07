a2bfe75eaa579a0a0889c096cb1410a780316b33
Flor Vázquez <arantxadlt@outlook.com>
2021-05-07 01:16:55 -0500

    Inicio de la selección de datos abiertos de México Covid-19, por estado generales y por estado por día

---
+++ SelecDatosAbiertos.R
@@ -0,0 +1,20 @@
+
+# Librerias ---------------------------------------------------------------
+#Ciencia de datos
+library(tidyverse)
+library(tsibble)
+
+#importación y lectura
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+# Importación de datos  ---------------------------------------------------
+
+
+# Limpieza de datos  ------------------------------------------------------
+
+
+# Tibble ------------------------------------------------------------------
+
+
